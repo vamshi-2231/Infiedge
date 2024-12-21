6561653b3549612899ac0291b64673e5a7c829c0
Mamindla Vamshi <102485178+vamshi-2231@users.noreply.github.com>
2024-12-21 23:09:47 +0530

    final6_commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -185,8 +185,6 @@ export default function Footer() {
                   <li><a href="https://www.linkedin.com/in/infiedge-software-bab851342/" className="social-link"><ion-icon name="logo-linkedin"></ion-icon></a></li>
                   <li><a href="https://x.com/Infiedge?t=rUcmgIDJ2eQ8UKAcK_ZYVA&s=08" className="social-link"><ion-icon name="logo-twitter"></ion-icon></a></li>
                   <li><a href="https://www.instagram.com/infiedge_software/" className="social-link"><ion-icon name="logo-instagram"></ion-icon></a></li>
-
-                  
                 </ul>
               </li>
             </ul>
